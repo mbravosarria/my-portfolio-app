bb07a736c822d8ba623f19a9c0de51f3393bd232
mbravosarria <mbravosarria@gmail.com>
2023-01-25 20:43:24 -0500

    feat: Data definition

---
+++ src/db/personal-info.ts
@@ -0,0 +1,185 @@
+export const PersonalInfo = {
+  name: 'Michel Bravo',
+  headline: 'Software Engineer',
+  summary: `Creative software engineer with two years of  experience as Web Developer and IT Specialist.
+            Proactive solving problems and contributing to project improvement. Motivated to advance and 
+            expand my skill set through challenging projects.  Strong skills include frontend development 
+            with React.js or Vue.js and backend development with Express.js.`,
+  languages: [
+    {
+      title: 'English',
+      percent: '50%',
+    },
+    {
+      title: 'Spanish',
+      percent: '100%',
+    },
+  ],
+}
+
+export const ContactInfo = {
+  address: '<<NOT DEFINED>>',
+  city: 'Lake Charles',
+  state: 'LA',
+  postal_code: '<<12345>>',
+  phone: '<<(123) 456-7890>>',
+  email: 'mbravosarria@gmail.com',
+  links: [
+    {
+      text: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/michel-bravo-sarria-870b38185/',
+    },
+    {
+      text: 'GitHub',
+      link: 'https://github.com/mbravosarria',
+    },
+  ],
+}
+
+export const WorkExperience = [
+  {
+    company: 'Cuspinera SURL',
+    location: 'Havana, Cuba',
+    location_type: 'Remote',
+    job_title: 'Frontend Developer',
+    employment_type: 'Freelance',
+    start_at: 'OCT 2022',
+    end_at: 'JAN 2023',
+    skills: ['Tailwind CSS', 'ECommerce', 'Node.js', 'React.js', 'Next.js'],
+  },
+  {
+    company: 'Telecommunications Company of Cuba S.A (ETECSA)',
+    location: 'Havana, Cuba',
+    location_type: 'Hybrid',
+    job_title: 'Full-stack Developer',
+    employment_type: 'Full Time',
+    start_at: 'MAY 2022',
+    end_at: 'DEC 2022',
+    skills: ['Node.js', 'React.js', 'Next.js', 'NestJS', 'MongoDB'],
+  },
+  {
+    company: 'Information Technology and Automation Company (ATI)',
+    location: 'Havana, Cuba',
+    location_type: 'On-site',
+    job_title: 'IT Specialist',
+    employment_type: 'Full Time',
+    start_at: 'NOV 2020',
+    end_at: 'MAY 2022',
+    skills: [
+      'Network Administration',
+      'Technical Support',
+      'Microsoft Office',
+      'Active Directory',
+      'Database Administration',
+    ],
+  },
+  {
+    company: 'Group of Entrepreneurs "Golden bulls"',
+    location: 'Havana, Cuba',
+    location_type: 'Remote',
+    job_title: 'Full-stack Developer',
+    employment_type: 'Freelance',
+    start_at: 'MAR 2021',
+    end_at: 'FEB 2022',
+    skills: ['Vue.js', 'Express.js', 'MySQL', 'JavaScript', 'Node.js'],
+  },
+]
+
+export const Projects = []
+
+export const HardSkills = [
+  'Web Development',
+  'Data Structure',
+  'Node.js',
+  'Database Management',
+  'Technical Support',
+]
+
+export const SoftSkills = [
+  'Creative Thinking',
+  'Communication',
+  'Time-Management',
+  'Research',
+  'Team Work',
+]
+
+export const TechnicalSkills = [
+  {
+    title: 'Frontend Development',
+    skills: [
+      'HTML',
+      'CSS',
+      'SASS',
+      'JavaScript',
+      'React.js',
+      'Next.js',
+      'Vue.js',
+      'Tailwind CSS',
+      'Material UI',
+      'Bootstrap',
+    ],
+  },
+  {
+    title: 'Back-end Development',
+    skills: [
+      'Rest API',
+      'Graphql',
+      'Express.js',
+      'Nest.js',
+      'React.js',
+      'Next.js',
+      'Vue.js',
+      'Mongodb',
+      'Mongoose',
+      'Mysql',
+      'Type ORM',
+    ],
+  },
+  {
+    title: 'IT Specialist',
+    skills: [
+      'Microsoft Office',
+      'Network Administration',
+      'Active Directory',
+      'Database Administration',
+    ],
+  },
+  {
+    title: 'Others',
+    skills: [
+      'Git',
+      'UI Mockup',
+      'Responsive Design',
+      'Netlify Deployment',
+      'Quality Assurance (QA)',
+    ],
+  },
+]
+
+export const Education = {
+  university:
+    'Technological University of Havana"José Antonio Echeverría"​, CUJAE',
+  location: 'Havana, Cuba',
+  degree: 'Computer Software Engineering',
+  start_at: 'SEP 2015',
+  end_at: 'NOV 2020',
+  description: {
+    title: 'Completion of studies',
+    text: 'Library of visual components of teaching calendars for the web.',
+  },
+}
+
+export const Certifications = [
+  {
+    title: 'JavaScript Algorithms and Data Structures',
+    owner: 'freeCodeCamp',
+    issued: 'JAN 2023',
+    link: 'https://www.freecodecamp.org/certification/fcc2c5d576f-c274-4243-977f-1e8286f8dfdc/javascript-algorithms-and-data-structures',
+  },
+  {
+    title: 'Responsive Web Design',
+    owner: 'freeCodeCamp',
+    issued: 'JAN 2023',
+    link: 'https://www.freecodecamp.org/certification/fcc2c5d576f-c274-4243-977f-1e8286f8dfdc/responsive-web-design',
+  },
+]
